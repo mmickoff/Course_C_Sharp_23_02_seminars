0166ff572d9f438d004f0282c82617984e1c9574
Mmick <mickmail2000@gmail.com>
2023-02-25 00:44:56 +0300

    Create & solved Task_1_1

---
+++ Lesson_1/1_1/Program.cs
@@ -0,0 +1,18 @@
+﻿// 01. Напишите программу, которая 
+// 1. на вход принимает два
+// числа,
+// 2.  второе чоисло возводит в квадрат,
+// 3. проверяет, является-ли первое число квадратом
+// второго.
+// а = 25, b = 5 -> да 
+// а = 2, b = 10 -> нет 
+// а = 9, b = -3 -> да 
+// а = -3 b = 9 -> нет
+
+Console.WriteLine("Необходимо ввести два числа. Введите первое число");
+double num1 = double.Parse(Console.ReadLine());
+Console.WriteLine("Введите вторвое число");
+double num2 = double.Parse(Console.ReadLine());
+double square = /* double.Parse( */Math.Pow(num2, 2)/* ) */;
+if (num1 == square) Console.WriteLine($"a = {num1}, b = {num2} -> да");
+else {Console.WriteLine($"a = {num1}, b = {num2} -> нет");}
