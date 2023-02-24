f91b9cc8b5e7ae287eeac5a27e4f4763b7cc6b1e
Mmick <mickmail2000@gmail.com>
2023-02-25 00:44:56 +0300

    Created & solved Task_1_1

--- Lesson_1/1_0/Program.cs
+++ Lesson_1/1_0/Program.cs
@@ -1,2 +1,10 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Task_1_0
+// 0. Демонстрация решения
+// Напишите программу, которая 
+// 1. на вход принимает число 
+// 2. выдаёт его квадрат (число умноженное на само себя).
+// Например: 4-> 16 -3-> 9 -7-> 49
+
+Console.WriteLine("Введите число");
+double number = double.Parse(Console.ReadLine());
+Console.WriteLine(Math.Pow(number, 2));
