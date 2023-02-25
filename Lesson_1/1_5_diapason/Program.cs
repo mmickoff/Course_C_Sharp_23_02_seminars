fb2695969ca1d70bb37a5321730ca83186551046
Mmick <mickmail2000@gmail.com>
2023-02-25 16:10:53 +0300

    Rename Fldrs by Task & Created & solved Task_1_5

---
+++ Lesson_1/1_5_diapason/Program.cs
@@ -0,0 +1,22 @@
+﻿// Task 1_5_diapason
+// 5. Напишите программу, которая 
+// на вход принимает одно число (N), а 
+// на выходе показывает все целые числа в промежутке от -N до N.
+// 4 -> "-4, -3, -2, -1, 0,1, 2, 3, 4" а
+// 2 -> " -2, -1, 0,1, 2"
+
+Console.WriteLine("Введите целое число");
+int num = int.Parse(Console.ReadLine()!);
+if (num < 1) Console.WriteLine("Не корректный ввод");
+else
+{
+    Console.Write($"{num} -> \"");
+    int minusNum = 0 - num;
+    while (minusNum <= num)
+    {
+        Console.Write(minusNum);
+        if (minusNum != num) Console.Write(", ");
+        else Console.WriteLine("\"");
+        minusNum++;
+    }
+}
