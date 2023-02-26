eac6a06017f79875a0fee891b5a54dea35a7824a
Mmick <mickmail2000@gmail.com>
2023-02-26 08:50:17 +0300

    Created & solved Task_1_4_maxNumber

---
+++ Lesson_1/1_4_maxNumber/Program.cs
@@ -0,0 +1,23 @@
+﻿// Задача 4: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+
+// 2, 3, 7 -> 7
+// 44 5 78 -> 78
+// 22 3 9 -> 22
+
+
+
+Console.WriteLine("Введите три числа для сравнения.\nВведите первое число:");
+double num1 = double.Parse(Console.ReadLine()!);
+Console.WriteLine("Введите второе число:");
+double num2 = double.Parse(Console.ReadLine()!);
+Console.WriteLine("Введите третье число:");
+double num3 = double.Parse(Console.ReadLine()!);
+Console.Write($"{num1} {num2} {num3} -> ");
+if (num1 == num2 || num2 == num3 || num3 == num1) Console.WriteLine("Некорректный ввод. Начните сначала и введите разные числа");
+else
+{
+    double max = num1;
+    if(num1 < num2) max = num2;
+    if(num2 < num3) max = num3; 
+    Console.WriteLine(max);
+}
---
+++ Lesson_1/1_6_evenNoEvenNumber/Program.cs
@@ -0,0 +1,13 @@
+﻿// Задача 6: Напишите программу, которая 
+// 1. на вход принимает число и 
+// 2. выдаёт, является ли число чётным (делится ли оно на два без остатка).
+// 4 -> да
+// -3 -> нет
+// 7 -> нет
+
+
+Console.WriteLine("Введите число");
+double number = double.Parse(Console.ReadLine()!);
+Console.Write(number + " -> ");
+if (number % 2 == 0) Console.WriteLine("Да");
+else Console.WriteLine("Нет");
