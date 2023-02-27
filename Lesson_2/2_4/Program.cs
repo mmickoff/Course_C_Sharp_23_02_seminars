9083d98a415cd973d7b6ccaa21e98fc93ca60aa2
Mmick <mickmail2000@gmail.com>
2023-02-27 11:58:53 +0300

    Cr&slvsd Tasks

---
+++ Lesson_2/2_4/Program.cs
@@ -0,0 +1,15 @@
+﻿// 4. Напишите программу, которая принимает на вход число и проверяет, кратно ли оно одновременно 7 и 23.
+Console.Write("Введите первое число: ");
+int N= int.Parse(Console.ReadLine()!);
+
+
+void Div(int N)
+    {
+        if (N % 7 ==0 & N % 23 ==0)
+            Console.WriteLine("Кратно");
+        else
+            Console.WriteLine("Не кратно ");
+    }
+
+
+Div(N);
