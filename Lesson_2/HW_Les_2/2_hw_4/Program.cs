a10a042e6729396680d7ca8f3151d62ff69723b2
mmick2000 <mickmail2000@gmail.com>
2023-02-28 10:42:02 +0300

    Create & Solved HW-Tasks 3&4

--- Lesson_2/HW_Les_2/2_hw_4/Program.cs
+++ Lesson_2/HW_Les_2/2_hw_4/Program.cs
@@ -4,3 +4,31 @@
 // 6 -> да
 // 7 -> да
 // 1 -> нет
+
+Console.WriteLine("Введите цифру, соответствующую дню недели");
+// int numberOfDay = int.Parse(Console.ReadLine()!);
+// int numOfWeekDay = numberOfDay;
+int numberOfDay = 0;
+int WeekDay(int numberOfDay){
+bool checkDay = false;
+while (checkDay == false){
+numberOfDay = int.Parse(Console.ReadLine()!);
+if (numberOfDay < 1 | numberOfDay > 7) {
+    Console.WriteLine ("Некорректный ввод - введите заново:");
+    checkDay = false;
+    }else{checkDay = true;}}
+return numberOfDay; }
+
+string FindDay(int numOfDay){
+    switch (numOfDay){
+        case 1: return "Нет";
+        case 2: return "Нет";
+        case 3: return "Нет";
+        case 4: return "Нет";
+        case 5: return "Нет";
+        case 6: return "Да";
+       default: return "Да";
+    }
+}
+int weekDay = WeekDay(numberOfDay);
+Console.WriteLine($"{weekDay} -> {FindDay(weekDay)}");
