4836fc88f2daf782d9382e6e69a62398966eb71a
mmickoff <mickmail2000@gmail.com>
2023-03-24 02:11:00 +0300

    Created & Solved Task 3 of HW_4

--- Lesson_4/HW_4/hw_4_3_1_CreateCopyArray/Program.cs
+++ Lesson_4/HW_4/hw_4_3_1_CreateCopyArray/Program.cs
@@ -7,3 +7,42 @@
 // 6,1,33,5,6,7,7,1 -> 6,1,33,5,6,7,7,1
 
 
+void FillArray(int[] arr, int startArr, int endArr, int lenArr)
+// чего-то у меня какой-то наворот с аргументами получился
+// пришлось вводить в аргументы массив, т.к. без него отказывается его считать
+// хотел обойтись только масиивом, но куда тогда втыкать запрашиваемые параметры???
+{
+    // int[] arr = new int[lenArr];
+    for (int i = 0; i < lenArr; i++)
+    {
+        arr[i] = new Random().Next(startArr, endArr);
+    }
+}
+
+void PrintArray(int[] arr)
+{
+    int count = arr.Length;
+    for (int i = 0; i < arr.Length; i++)
+    {
+        Console.Write(arr[i]);
+        if (i < arr.Length - 1) Console.Write(", ");
+    }
+}
+
+Console.WriteLine("Введите диапазон чисел, заполняющих массив - от нижего до верхнего предела,-и его размер. Начинайте с нижнего значения, потом верхнее, и затем размер массива:");
+int.TryParse(Console.ReadLine(), out int startArray);
+int.TryParse(Console.ReadLine(), out int endArray);
+int.TryParse(Console.ReadLine(), out int lengthArr);
+// int startArray = int.Parse(Console.ReadLine()!);
+// int endArray = int.Parse(Console.ReadLine()!);
+// int lengthArr = int.Parse(Console.ReadLine()!);
+// int startArray = Console.ReadLine();
+// int endArray = Console.ReadLine();
+// int lengthArr = Console.ReadLine();
+
+// ЭКСПЕРИМЕНТИРОВАЛ, Т.К. ERROR  НА ВСЕ ТРИ СТРОЧКИ ВЫДАВАЛО
+
+int[] arr = new int[lengthArr];
+
+FillArray(arr, startArray, endArray, lengthArr);
+PrintArray(arr);
